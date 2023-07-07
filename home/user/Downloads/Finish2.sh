d26d75ffa8701d121e3c4a816178d4cd209f055a
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2023-07-07 13:48:42 -0400

    Update Finish2.sh

--- home/user/Downloads/Finish2.sh
+++ home/user/Downloads/Finish2.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Cleanup the calling script
-#rm "/home/user/Desktop/Finish1.sh"
+rm "/home/user/Desktop/Finish1.sh"
 
 cp -f -R /home/user/Downloads/ThinClient/etc /
-sleep 10
-rm -f -R /home/user/Downloads/ThinClient
+sleep 1; rm -f -R /home/user/Downloads/ThinClient
 
 
 # Pulse VPN Client
 echo 'Installing Pulse Secure VPN Client'
-apt -y install '/home/user/Downloads/ps-pulse-linux-installer.deb'
-/opt/pulsesecure/bin/jamCommand /importfile /home/user/Downloads/EOTSS_Azure.pulsepreconfig
-/opt/pulsesecure/bin/setup_cef.sh install
-#rm /Downloads/ps-pulse-linux-installer.deb
-#rm /Downloads/EOTSS_Azure.pulsepreconfig
+apt -y install '/home/user/Downloads/ps-pulse-linux-installer.deb' >/dev/null
+/opt/pulsesecure/bin/jamCommand /importfile /home/user/Downloads/EOTSS_Azure.pulsepreconfig >/dev/null
+/opt/pulsesecure/bin/setup_cef.sh install >/dev/null
+rm /Downloads/ps-pulse-linux-installer.deb
+rm /Downloads/EOTSS_Azure.pulsepreconfig
 
 # VMware Horizon View
 echo 'Installing VMware Horizon View Client'
-echo "y" | '/home/user/Downloads/VMware-Horizon-Client.bundle' --console --required --stop-services
+echo "y" | '/home/user/Downloads/VMware-Horizon-Client.bundle' --console --required --stop-services >/dev/null
+rm /home/user/Downloads/VMware-Horizon-Client.bundle
 
 
 echo "Did it work?"
