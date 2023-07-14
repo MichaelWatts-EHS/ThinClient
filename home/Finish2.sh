c5a34b02b4f08fa131c08c6d77edc58210e3a616
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2023-07-14 14:27:29 -0400

    Update Finish2.sh

--- home/Finish2.sh
+++ home/Finish2.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
 # Cleanup the calling script
-rm "/home/user/Desktop/Finish1.sh"
+rm "/home/user/Downloads/Finish1.sh"
 
-cp -f -R /home/user/Downloads/ThinClient/etc /
-hostnamectl set-hostname thinclient
-rm -f -R /home/user/Downloads/ThinClient
-sed -i 's/"GRUB_TIMEOUT=5"/"GRUB_TIMEOUT=0"/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg >/dev/null
+#cp -f -R /home/user/Downloads/ThinClient/etc /
+#hostnamectl set-hostname thinclient
+#rm -f -R /home/user/Downloads/ThinClient
+#sed -i 's/"GRUB_TIMEOUT=5"/"GRUB_TIMEOUT=0"/' /etc/default/grub
+#grub-mkconfig -o /boot/grub/grub.cfg >/dev/null
 
 # Pulse VPN Client
-echo 'Installing Pulse Secure VPN Client'
-apt -y install '/home/user/Downloads/ps-pulse-linux-installer.deb' >/dev/null
-sleep 1
-/opt/pulsesecure/bin/jamCommand /importfile /home/user/Downloads/EOTSS_Azure.pulsepreconfig >/dev/null
-sleep 1
-/opt/pulsesecure/bin/setup_cef.sh install >/dev/null
-sleep 1
-rm '/home/user/Downloads/ps-pulse-linux-installer.deb' >/dev/null
-rm '/home/user/Downloads/EOTSS_Azure.pulsepreconfig' >/dev/null
+#echo 'Installing Pulse Secure VPN Client'
+#apt -y install '/home/user/Downloads/ps-pulse-linux-installer.deb' >/dev/null
+#sleep 1
+#/opt/pulsesecure/bin/jamCommand /importfile /home/user/Downloads/EOTSS_Azure.pulsepreconfig >/dev/null
+#sleep 1
+#/opt/pulsesecure/bin/setup_cef.sh install >/dev/null
+#sleep 1
+#rm '/home/user/Downloads/ps-pulse-linux-installer.deb' >/dev/null
+#rm '/home/user/Downloads/EOTSS_Azure.pulsepreconfig' >/dev/null
 
 # VMware Horizon View
-echo 'Installing VMware Horizon View Client'
-echo "y" | '/home/user/Downloads/VMware-Horizon-Client.bundle' --console --required --stop-services >/dev/null
-sleep 1
-rm '/home/user/Downloads/VMware-Horizon-Client.bundle' >/dev/null
+#echo 'Installing VMware Horizon View Client'
+#echo "y" | '/home/user/Downloads/VMware-Horizon-Client.bundle' --console --required --stop-services >/dev/null
+#sleep 1
+#rm '/home/user/Downloads/VMware-Horizon-Client.bundle' >/dev/null
 
-
-systemctl --no-wall reboot
-exit
-
-
+# Reboot
+#systemctl --no-wall reboot
+#exit
