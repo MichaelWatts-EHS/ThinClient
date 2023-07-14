a4424af4a40489c8fb921ec8c0dc099ce1f10a68
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2023-07-14 15:29:10 -0400

    Update Finish2.sh

--- home/Finish2.sh
+++ home/Finish2.sh
@@ -2,14 +2,14 @@
 
 echo "Running Finish2"
 
-echo "Running Finish2" > /home/user/Downloads/ThinClient/etc/test.txt
-
 # Cleanup the calling script
 rm Finish1.sh
 
 cp -f -R /home/user/Downloads/ThinClient/etc /
+
 sed -i 's/"GRUB_TIMEOUT=5"/"GRUB_TIMEOUT=0"/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg >/dev/null
+
 hostnamectl set-hostname thinclient
 
 # Pulse VPN Client
@@ -30,6 +30,8 @@ hostnamectl set-hostname thinclient
 #rm '/home/user/Downloads/VMware-Horizon-Client.bundle' >/dev/null
 
 #rm -f -R /home/user/Downloads/ThinClient
+
+read -n 1 -r -p "So far so good.  Press any key to reboot ..."
 
 # Reboot
 systemctl --no-wall reboot
