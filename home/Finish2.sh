c4126fe0684c1d51fb63ce7e03115969b5ebb4e8
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2023-07-17 11:39:55 -0400

    Update Finish2.sh

--- home/Finish2.sh
+++ home/Finish2.sh
@@ -3,9 +3,11 @@
 #echo "Running Finish2"
 
 # Cleanup the calling script
-if [ ! -f "/home/user/Finish1.sh" ]; then
-  rm -f "/home/user/Finish1.sh"
-fi
+rm -f "/home/user/Finish1.sh"
+
+#if [ ! -f "/home/user/Finish1.sh" ]; then
+#  rm -f "/home/user/Finish1.sh"
+#fi
 
 echo "Applying system configuration files"
 cp -f -R /home/user/Downloads/ThinClient/etc /
@@ -15,9 +17,10 @@ echo "Setting grub timeout"
 sed -i 's/"GRUB_TIMEOUT=5"/"GRUB_TIMEOUT=0"/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg 2>&1 </dev/null &
 
-#hostnamectl set-hostname thinclient
+echo "Setting hostname"
+hostnamectl set-hostname thinclient
 
-#apt update
+apt -y update
 
 # Install Pulse VPN Client
 #if [ ! -f /opt/pulsesecure/bin/pulseUI ]; then
